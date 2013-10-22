1631c736acb1f290ac5100ec402cae9873ab919e
hasegawa-takasei <hasegawa_takasei@cygamses.co.jp>
2013-10-22 15:50:02 +0900

    異常チェックルーチン追加

--- load-largefile.py
+++ load-largefile.py
@@ -11,6 +11,15 @@ BASE_DIR = path.path('~/.gitasset').expanduser()
 DATA_DIR = BASE_DIR / 'data'
 
 hd = os.fdopen(sys.stdin.fileno(), 'rb').read()
-dirpath = DATA_DIR / hd[:2] / hd[2:4]
-filepath = dirpath / hd[4:]
-sys.stdout.write(filepath.bytes())
+if len(hd) != 40:
+	sys.stdout.write(hd)
+elif hd.isalnum() == False:
+	sys.stdout.write(hd)
+else:
+	dirpath = DATA_DIR / hd[:2] / hd[2:4]
+	filepath = dirpath / hd[4:]
+	if os.path.exists(filepath):
+		sys.stdout.write(filepath.bytes())
+	else:
+		sys.stdout.write(hd)
+
